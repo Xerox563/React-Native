057ed4b0fe44afe7f9e511d3ad37842f30a47036
Amit Gangwar <gangwaramit449@gmail.com>
2025-01-17 09:54:15 +0530

    Started Building

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+/*
+- cd native-app
+- yarn start # you can open iOS, Android, or web from here, or run them directly with the commands below.
+- yarn android
+- yarn ios # requires an iOS device or macOS for access to an iOS simulator
+- yarn web
+ */
